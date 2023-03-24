752e1374af9fb9e807917a27441dce417378ca44
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2023-03-24 16:59:43 -0400

    Created boilerplate, webpack, json, installed dependencies This is the body of the commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }],
+                            ['@babel/preset-react', { targets: 'defaults' }],
+                        ],
+                    }
+                }
+            }, 
+            {
+                test:/\.s[ac]ss$/i,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: './index.html',
+          }),
+    ],
+    devServer: {
+        static: {
+          publicPath: '/build',
+          directory: path.resolve(__dirname, 'build'),
+        },
+        proxy: {
+          '/trips': 'http://localhost:3000/',
+        },
+				compress: true,
+				port: 8080,
+      },
+}
