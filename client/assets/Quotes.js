9d9ddc918acae521459751480e5d6337690f6c19
Michael Capone <mcapone6@gatech.edu>
2023-03-28 19:10:33 -0400

    Basic implementation of the updated frontend

---
+++ client/assets/Quotes.js
@@ -0,0 +1,24 @@
+const vitaminDQuotes = [
+    "I don't always get my vitamin D, but when I do, I prefer to get it from the sun.",
+    "Vitamin D deficiency is no laughing matter, unless you're a clown.",
+    "I never realized how much I needed vitamin D until I started dating someone who's always sunny.",
+    "If life gives you lemons, make lemonade. If life gives you sunshine, soak up some vitamin D!",
+    "Vitamin D is like happiness - you can never have too much of it.",
+    "I used to think vitamin D was just a letter of the alphabet. Now I know it's the key to a good mood.",
+    "The best things in life are free: hugs, laughter, and vitamin D.",
+    "I don't need a daily dose of vitamin D, I need a daily dose of vitamin sea.",
+    "They say an apple a day keeps the doctor away, but I think a little sunshine and vitamin D can't hurt either.",
+    "I don't always get enough vitamin D, but when I do, I feel like I can conquer the world.",
+    "If you're feeling down, just remember that you're one vitamin D away from a better mood.",
+    "Some people need coffee to start their day. I just need a little sunshine and a dose of vitamin D.",
+    "They say laughter is the best medicine, but have they tried a good dose of vitamin D?",
+    "I don't always get enough vitamin D, but when I do, I feel like a ray of sunshine.",
+    "Vitamin D deficiency is like a cloud hanging over your head. So go outside and get some sunshine, and let that cloud disperse!",
+    "I used to think I was solar-powered, but it turns out I just need my daily dose of vitamin D.",
+    "If you're not getting enough vitamin D, don't worry. Just fake it 'til you make it and wear some sunglasses.",
+    "Why did the vitamin D cross the road? To get to the sunny side!",
+    "A little bit of vitamin D goes a long way. So go outside, get some sunshine, and soak up that good mood!",
+    "I'm not addicted to vitamin D, I'm just a little bit photosynthesis-challenged."
+  ];
+  
+export default vitaminDQuotes;
--- client/components/BigButton.jsx
+++ client/components/BigButton.jsx
@@ -1,4 +1,12 @@
 import React, { useState, useEffect } from "react";
+import Snackbar from '@mui/material/Snackbar';
+import Slide from '@mui/material/Slide';
+import vitaminDQuotes from '../assets/Quotes.js';
+
+
+function getRandomQuote() {
+  return vitaminDQuotes[Math.floor((Math.random() * vitaminDQuotes.length))];
+}
 
 function BigButton(props) {
   //Send to backend:
@@ -21,6 +29,18 @@ function BigButton(props) {
       });
   };
 
+  const getUpdatedPoints = () => {
+    if(isOutside) {
+      fetch("/api/getPoints")
+        .then((response) => response.json())
+        .then((response) => {
+          updatePoints(response);
+        });
+    }
+  }
+
+  const [isSnackOpen, setIsSnackOpen] = useState(false);
+  const [message, setMessage] = useState(getRandomQuote())
   const [isOutside, switchOutside] = useState(false);
   const [startTime, setStart] = useState(0);
   const [totalPoints, updatePoints] = useState(0);
@@ -35,6 +55,8 @@ function BigButton(props) {
           updatePoints(response.points);
         }
       });
+
+      // setTimeout(getUpdatedPoints, 1000);
   });
 
   const handleClick = () => {
@@ -44,11 +66,18 @@ function BigButton(props) {
       const points = props.uv * totalMinutes;
 
       addSession(props.username, new Date().toDateString(), points);
+      setIsSnackOpen(true);
     } else {
       setStart(Date.now());
+      setIsSnackOpen(false);
+      setMessage(getRandomQuote());
     }
     switchOutside(!isOutside);
   };
+
+  function TransitionDown(props) {
+    return <Slide {...props} direction="down" />;
+  }
 
   return (
     <div id="d-meter">
@@ -62,6 +91,14 @@ function BigButton(props) {
           ? "YOU'RE OUTSIDE! GO INSIDE?"
           : "YOU'RE INSIDE! GO OUTSIDE?"}
       </button>
+      <Snackbar
+        anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+        open={isOutside}
+        TransitionComponent={TransitionDown}
+        autoHideDuration={5000}
+        message={message}
+        onClose={() => console.log('Active')}
+      />
     </div>
   );
 }
