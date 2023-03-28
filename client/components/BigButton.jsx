ccdb667c092d0ea1f32aa680769e35911711f246
Sidney Brodsky <sidneybrodsky@sidneys-air.mynetworksettings.com>
2023-03-28 15:54:24 -0400

    updates progress bar and login

--- client/components/BigButton.jsx
+++ client/components/BigButton.jsx
@@ -1,69 +1,70 @@
 import React, { useState, useEffect } from "react";
 
 function BigButton(props) {
-  //Send to backend:
-  // Make request to /api/submit
-  // POST
-  // BODY: {username: 'username', date: 'string', points: 'number'}
-  // response.body: number
+	const addSession = (username) => {
+		fetch("/api/submit", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(
+				{
+					username,
+					date: new Date().toDateString(),
+					startTime,
+					endTime: currentTime,
+					points: currentPoints
+				}
+			),
+		})
+			.then(res => res.json())
+			.then(console.log)
+			.catch(console.log)
+	};
 
-  const addSession = (username, date, points) => {
-    fetch("/api/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username: username, date: date, points: points }),
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        updatePoints(response);
-      });
-  };
+	const [isOutside, setIsOutside] = useState(false);
+	const [startTime, setStartTime] = useState(Date.now());
+	const [previousPoints, setPreviousPoints] = useState(0);
+	const [currentPoints, setCurrentPoints] = useState(0);
 
-  const [isOutside, switchOutside] = useState(false);
-  const [startTime, setStart] = useState(0);
-  const [totalPoints, updatePoints] = useState(0);
+	useEffect(() => {
+		if (isOutside) {
+			const interval = setInterval(() => {
+				const currentTime = Date.now();
+				const elapsedMinutes = (currentTime - startTime) / 60000;
+				const points = previousPoints + props.uv * elapsedMinutes;
+				setCurrentPoints(points);
+			}, 1000);
+			return () => clearInterval(interval);
+		}
+	}, [isOutside, startTime, previousPoints, props.uv]);
 
-  useEffect(() => {
-    fetch(`/api/submit/${props.username}`)
-      .then((response) => response.json())
-      .then((response) => {
-        if (!response) {
-          updatePoints(0);
-        } else {
-          updatePoints(response.points);
-        }
-      });
-  });
+	const handleButtonClick = () => {
+		if (isOutside) {
+			const currentTime = Date.now();
+			const elapsedMinutes = (currentTime - startTime) / 60000;
+			const points = previousPoints + props.uv * elapsedMinutes;
+			setPreviousPoints(points);
+			setStartTime(null);
+			addSession(props.username, points);
+		} else {
+			setStartTime(Date.now());
+		}
+		setIsOutside(!isOutside);
+	};
 
-  const handleClick = () => {
-    if (isOutside === true) {
-      const totalMinutes = (Date.now() - startTime) / 60000;
-      setStart(0);
-      const points = props.uv * totalMinutes;
-
-      addSession(props.username, new Date().toDateString(), points);
-    } else {
-      setStart(Date.now());
-    }
-    switchOutside(!isOutside);
-  };
-
-  return (
-    <div id="d-meter">
-      <div>D-Meter: {totalPoints.toFixed(2)}%</div>
-      <div id="progress-container">
-        <div id="loading" style={{ width: `${totalPoints}%` }}></div>
-      </div>
-      <br />
-      <button id="big-button" onClick={handleClick}>
-        {isOutside
-          ? "YOU'RE OUTSIDE! GO INSIDE?"
-          : "YOU'RE INSIDE! GO OUTSIDE?"}
-      </button>
-    </div>
-  );
+	return (
+		<div id="d-meter">
+			<div>D-Meter: {currentPoints.toFixed(2)}%</div>
+			<div id="progress-container">
+				<div id="loading" style={{ width: `${currentPoints}%` }}></div>
+			</div>
+			<br />
+			<button id="big-button" onClick={handleButtonClick}>
+				{isOutside ? "YOU'RE OUTSIDE! GO INSIDE?" : "YOU'RE INSIDE! GO OUTSIDE?"}
+			</button>
+		</div>
+	);
 }
 
 export default BigButton;
--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,41 +1,189 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import {
+	Button,
+	CssBaseline,
+	TextField,
+	FormControlLabel,
+	Checkbox,
+	Link,
+	Grid,
+	Box,
+	Typography,
+	Avatar,
+} from '@material-ui/core';
+import logo from '../../images/logo.png';
 
-function Login() {
-  const [zipcodeEntry, zipcodeUpdate] = useState(0);
-  const [nameEntry, nameUpdate] = useState("");
+const styles = {
+	paper: {
+		marginTop: '8px',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	form: {
+		width: '100%',
+		marginTop: '3px',
+	},
+	submit: {
+		margin: '3px 0 2px',
+	},
+};
 
-  const handleChange = () => {
-    zipcodeUpdate(document.getElementById("zipcodeInput").value);
-    nameUpdate(document.getElementById("nameInput").value.toUpperCase());
-  };
-  return (
-    <div id="loginElements">
-      <form className="flex">
-        <input
-          id="nameInput"
-          name="username"
-          placeholder="Name"
-          onChange={handleChange}
-        ></input>
-        <input
-          id="zipcodeInput"
-          name="zipcode"
-          placeholder="Zip Code"
-          onChange={handleChange}
-        ></input>
-      </form>
+const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [isSigningUp, setIsSigningUp] = useState(false); // Track whether user is signing up or not
+	const [name, setName] = useState('');
 
-      <button id="loginButton">
-        <Link
-          to="/home"
-          state={{ zipcodeEntry: zipcodeEntry, nameEntry: nameEntry }}
-        >
-          <div className="sunnyD-img"></div>
-        </Link>
-      </button>
-    </div>
-  );
-}
+	const handleLogin = (event) => {
+		event.preventDefault();
+		const body = JSON.stringify({
+			email,
+			password
+		})
+		fetch('/api/login', { method: 'POST', body })
+	};
+
+	const handleSignUp = (event) => {
+		event.preventDefault();
+		const body = JSON.stringify({
+			email,
+			password,
+			name
+		})
+		fetch('/api/signup', { method: 'POST', body })
+	};
+
+	const handleSignUpClick = () => {
+		setIsSigningUp(true); // Display sign up form on click
+	};
+
+	const handleCancelClick = () => {
+		setIsSigningUp(false); // Display login form on cancel click
+	};
+
+	return (
+		<div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+			<CssBaseline />
+			<div style={styles.paper}>
+				<Typography component="h1" variant="h5">
+					{isSigningUp ? 'Sign up' : 'Sign in'}
+				</Typography>
+				{isSigningUp ? (
+					<form style={styles.form} onSubmit={handleSignUp}>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							id="name"
+							label="Name"
+							name="name"
+							autoComplete="name"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="new-password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							style={styles.submit}
+						>
+							Sign up
+						</Button>
+						<Grid container>
+							<Grid item>
+								<Link href="#" variant="body2" onClick={handleCancelClick}>
+									{'Cancel'}
+								</Link>
+							</Grid>
+						</Grid>
+					</form>
+				) : (
+					<form style={styles.form} onSubmit={handleLogin}>
+						<TextField
+							variant="outlined"
+
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							autoFocus
+						/>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<FormControlLabel
+							control={<Checkbox value="remember" color="primary" />}
+							label="Remember me"
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							style={styles.submit}
+						>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href="#" variant="body2">
+									Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href="#" variant="body2" onClick={handleSignUpClick}>
+									{"Don't have an account? Sign Up"}
+								</Link>
+							</Grid>
+						</Grid>
+					</form>
+				)}
+			</div>
+		</div>
+	);
+};
 
 export default Login;
